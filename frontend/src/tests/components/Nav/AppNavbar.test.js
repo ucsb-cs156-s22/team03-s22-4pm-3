28bac4b565191e8398c2035e561a9cf3f9fa4a29
Calvin Dougher <cwdougher@umail.ucsb.edu>
2022-05-17 18:21:37 -0700

    Removed Test

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -373,33 +373,4 @@ describe("AppNavbar tests", () => {
       expect(getByTestId(/appnavbar-dining-commons-list/)).toBeInTheDocument()
     );
   });
-  test("renders the reviews menu correctly for an admin", async () => {
-    const currentUser = currentUserFixtures.adminUser;
-    const systemInfo = systemInfoFixtures.showingBoth;
-
-    const doLogin = jest.fn();
-
-    const { getByTestId } = render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <AppNavbar
-            currentUser={currentUser}
-            systemInfo={systemInfo}
-            doLogin={doLogin}
-          />
-        </MemoryRouter>
-      </QueryClientProvider>
-    );
-
-    await waitFor(() =>
-      expect(getByTestId("appnavbar-reviews-dropdown")).toBeInTheDocument()
-    );
-    const dropdown = getByTestId("appnavbar-reviews-dropdown");
-    const aElement = dropdown.querySelector("a");
-    expect(aElement).toBeInTheDocument();
-    aElement?.click();
-    await waitFor(() =>
-      expect(getByTestId(/appnavbar-reviews-list/)).toBeInTheDocument()
-    );
-  });
 });
