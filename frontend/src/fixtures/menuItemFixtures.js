87e37ae82871e113565ae544eb70ce9d176c29d0
KelvinZhang123 <kelvin_zhang@umail.ucsb.edu>
2022-05-18 02:29:46 -0700

    Added menuItem table indexpage and fixture

---
+++ frontend/src/fixtures/menuItemFixtures.js
@@ -0,0 +1,31 @@
+const menuitemFixtures = {
+    onemenuitem: {
+            "id": 1,
+            "diningCommonsCode": "ortega",
+            "name": "Tofu Banh Mi",
+            "station": "Entree"
+    },
+    threemenuitem: [
+        {
+            "id": 2,
+            "diningCommonsCode": "portola",
+            "name": "Taco",
+            "station": "Food"
+            },
+        {
+            "id": 3,
+            "diningCommonsCode": "carillo",
+            "name": "Stir Fry",
+            "station": "Grill"
+        },
+        {
+            "id": 4,
+            "diningCommonsCode": "ortega",
+            "name": "Burrito",
+            "station": "Entree"
+        } 
+    ]
+};
+
+
+export { menuitemFixtures };
