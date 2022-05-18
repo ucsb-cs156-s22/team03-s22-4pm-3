d6d99404f67fadf94a93493f1d1b561f3117c775
Calvin Dougher <cwdougher@umail.ucsb.edu>
2022-05-17 17:59:42 -0700

    Articles Frontend Implemented

---
+++ frontend/src/main/pages/Articles/ArticlesIndexPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useBackend } from "main/utils/useBackend"; // use prefix indicates a React Hook
+
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useCurrentUser } from "main/utils/currentUser"; // use prefix indicates a React Hook
+import ArticlesTable from "main/components/Articles/ArticlesTable";
+
+export default function ArticlesIndexPage() {
+  const currentUser = useCurrentUser();
+
+  const {
+    data: articles,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/Article/all"],
+    // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+    { method: "GET", url: "/api/Article/all" },
+    []
+  );
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Articles</h1>
+        <ArticlesTable articles={articles} currentUser={currentUser} />
+      </div>
+    </BasicLayout>
+  );
+}
